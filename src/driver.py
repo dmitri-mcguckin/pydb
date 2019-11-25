bde297eb8f4593653d2d11be431f607aaf4bd901
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2019-11-25 14:43:12 -0800

    added client stuff

--- src/driver.py
+++ src/driver.py
@@ -2,20 +2,59 @@ import psycopg2, os, sys
 from ftfutils import *
 
 DEBUG = False
+COL_WIDTH=20
+FORMAT_STRING='{0: <' + str(COL_WIDTH) + '}'
+
+# dbclass.cs.pdx.edu
 
 def main():
+    # Check for cli arguments
+    if(len(sys.argv) !=3):
+        log(Mode.ERROR, "Usage: python3 driver.py [host] \"<host>\"")
+        exit(-1)
+
     # Get login info from environment variables
     db_user = os.getenv('PYDB_USER')
     db_password = os.getenv('PYDB_PASSWORD')
 
     # Get host info from user
-    uarg = sys.argv[1].split(':')
-    db_host = uarg[0]
-    db_port = uarg[1]
+    db_host = sys.argv[1]
 
-    log(Mode.INFO, "Connecting to database: " + db_user + "@" + db_host + ":" + db_port)
-    if(DEBUG): log(Mode.DEBUG, "Hash: " + db_password)
+    # Open the session
+    session = psycopg2.connect(database=db_user, user=db_user, password=db_password, host=db_host)
+    db = session.cursor()
+    if(DEBUG): log(Mode.DEBUG, "Opened session to database: " + str(session))
 
-    psycopg2.connect(database="spy", user=db_user, password=db_password, host=db_host, port=db_port)
+    # Make the query
+    query = sys.argv[2]
+    try:
+        db.execute(query)
+    except Exception as e:
+        log(Mode.ERROR, str(e))
+        session.close()
+        if(DEBUG): log(Mode.DEBUG, "Closed session to database: " + str(session))
+        exit(-1)
+
+    cols = [desc[0] for desc in db.description]
+    rows = db.fetchall()
+
+    # Close the session
+    session.close()
+    if(DEBUG): log(Mode.DEBUG, "Closed session to database: " + str(session))
+
+    for col in cols:
+        print(FORMAT_STRING.format(col), end="")
+    print("\n", end="")
+
+    for col in cols:
+        for i in range(0, COL_WIDTH): print("-", end="")
+    print("\n", end="")
+
+    for row in rows:
+        for i in row:
+            print(FORMAT_STRING.format(i), end="")
+        print("\n", end="")
+
+    log(Mode.INFO, str(len(rows)) + " Results.")
 
 if __name__ == '__main__': main()
